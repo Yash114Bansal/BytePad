d4ca769d39656d0b783bcd61ffc5b0f81bc1b20d
yash114bansal <yash114bansal@gmail.com>
2023-10-21 15:50:07 +0530

    add user and faculty models

---
+++ accounts/choices.py
@@ -0,0 +1,26 @@
+# choices.py in your app
+SEMESTER_CHOICES = (
+    (1, 'Semester 1'),
+    (2, 'Semester 2'),
+    (3, 'Semester 3'),
+    (4, 'Semester 4'),
+    (5, 'Semester 5'),
+    (6, 'Semester 6'),
+    (7, 'Semester 7'),
+    (8, 'Semester 8'),
+)
+
+BRANCH_CHOICES = (
+    ('CSE', 'Computer Science and Engineering'),
+    ('ECE', 'Electronics and Communication Engineering'),
+    ('ME', 'Mechanical Engineering'),
+    ('CE', 'Civil Engineering'),
+    ('CSEAIML', 'Computer Science and Engineering (AI/ML)'),
+    ('CSEDS', 'Computer Science and Engineering (Data Science)'),
+    ('IT', 'Information Technology'),
+    ('CS', 'Computer Science'),
+    ('AIML', 'Artificial Intelligence and Machine Learning'),
+    ('CSIT', 'Computer Science and Information Technology'),
+    ('EN', 'Electrical And Electronics Engineering'),
+    ('CSEH', 'Computer Science and Engineering (Hindi)'),
+)
--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,46 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from .managers import UserManager
+from .choices import *
 
 class UserProfile(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     email = models.EmailField(unique=True)
+
     is_department_head = models.BooleanField(default=False)
     is_faculty = models.BooleanField(default=False)
     is_student = models.BooleanField(default=True)
 
     objects = UserManager()
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    branch = models.CharField(max_length=8, choices=BRANCH_CHOICES)
+    semester = models.IntegerField(choices=SEMESTER_CHOICES)
+
+
+class StudentModel(models.Model):
+
+    student_number = models.IntegerField(unique=True)
+    roll_number = models.IntegerField(unique=True)
+    current_semester = models.IntegerField(choices=SEMESTER_CHOICES)
+    branch = models.CharField(max_length=8, choices=BRANCH_CHOICES)
+    contact_number = models.IntegerField()
+    date_of_birth = models.DateField()
+    guardian_name = models.CharField(max_length=100)
+    guardian_contact_number = models.IntegerField()
+    courses = models.ManyToManyField(Course)
+
+class FacultyModel(models.Model):
+
+    employee_id = models.CharField(max_length=10, unique=True)
+    contact_number = models.IntegerField()
+    date_of_birth = models.DateField()
+    courses = models.ManyToManyField(Course)
+    department = models.CharField(max_length=8, choices=BRANCH_CHOICES)
+    is_department_head = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.employee_id
