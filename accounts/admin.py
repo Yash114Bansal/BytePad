7099ce0f7c65e45f2c54c95479981a72eb7b5434
yash114bansal <yash114bansal@gmail.com>
2023-10-22 21:08:19 +0530

    add batch to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Course,StudentModel,FacultyModel,UserProfile
+from .models import Course,StudentModel,FacultyModel,UserProfile,Batch
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Course)
 admin.site.register(StudentModel)
 admin.site.register(FacultyModel)
+admin.site.register(Batch)
